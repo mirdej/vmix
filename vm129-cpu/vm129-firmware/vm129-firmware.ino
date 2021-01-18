d36d29c4ab23615d401aa9ec3e93bced1dce2cb6
Michael Egger <me@anyma.ch>
2021-01-18 20:37:49 +0100

    buttons, leds and basic menus

--- vm129-cpu/vm129-firmware/vm129-firmware.ino
+++ vm129-cpu/vm129-firmware/vm129-firmware.ino
@@ -82,11 +82,20 @@ const int SYNC_MODE_EXTERN_AUDIO    = 1;
 const int SYNC_MODE_EXTERN_MIDI     = 2;
 
 const int MENU_NONE			        = 0;
+const int MENU_BRIGHTNESS			= 1;
+const int MENU_BPM			        = 2;
+const int MENU_TRIGGER_1			= 11;
+const int MENU_TRIGGER_2			= 12;
+const int MENU_TRIGGER_3			= 13;
+
+const int trigger_clocks[] = {6,12,24,48,96};
+char *trigger_strings[] = { "16th", "8th",  "beats","half","bar"};
 
 
 const int 	UI_TIMEOUT = 5000;
 
 #define NUM_REGISTERS   3
+#define SELECTION_COLOR             CRGB::Gray
 // ..................................................................................... SCREEN
 
 
@@ -110,6 +119,7 @@ int                                     sync_mode;
 int                                     bpm;
 int                                     bpm_displayed;
 
+int                                     brightness;
 
 long                                    last_ui_interaction;
 long                                    last_beat;
@@ -119,9 +129,10 @@ volatile int                            interruptCounter;
 int                                     tickCounter;
  
 const int                               beat_divider = 24;      //MIDI CLOCk Ticks per beat
+
 int                                     beatclock_clocks = beat_divider;         // default output beat on trigger out 1 
-int                                     trigger1_clocks = beat_divider;         // default output beat on trigger out 1 
-int                                     trigger2_clocks = beat_divider * 4;     // default output bar on trigger out 2 
+int                                     trigger1_clocks = beat_divider * 4 ;      // default output bar on trigger out 1 
+int                                     trigger2_clocks = beat_divider ;        // default output beat on trigger out 2
 int                                     trigger3_clocks = beat_divider / 4;     // default output 16ths on trigger out 3
  
  
@@ -131,7 +142,7 @@ portMUX_TYPE                            beatClockTimerMux = portMUX_INITIALIZER_
 bool                                    display_needs_update;
 
 
-byte buttons_raw[NUM_REGISTERS];
+long buttons_raw;
 
 //----------------------------------------------------------------------------------------
 //																		Intro
@@ -198,37 +209,100 @@ void check_ui_timeout() {
 //----------------------------------------------------------------------------------------
 //																		Encoder
 void check_encoder(){
-    encoder.getCount();
+    if (encoder.getCount() == 0) return;
     
+    
+	switch(selected_menu) {
+		case MENU_NONE:
+		    break;
+		case MENU_BRIGHTNESS:
+            brightness +=     encoder.getCount();
+	    	if (brightness < 5) brightness = 5;
+            if (brightness > 255)brightness = 255;
+            FastLED.setBrightness(brightness);
+            FastLED.show();
+            display_needs_update = true;
+		    break;
+		case MENU_BPM:
+            bpm +=     encoder.getCount();
+            if (bpm < 60)   bpm = 60;
+            if (bpm > 240)  bpm = 240;
+            display_needs_update = true;
+            change_bpm();
+		    break;
+		default:
+		    break;
+	}
+
+    last_ui_interaction = millis();
     encoder.setCount(0);
 }
 
 //----------------------------------------------------------------------------------------
 //																		Buttons
 void check_buttons() {
-    static byte old_buttons[NUM_REGISTERS];
-    bool    something_happened = false;
+    static long old_buttons;
+
     byte temp;
+    buttons_raw = 0;
     
-    SPI.beginTransaction(SPISettings(80000, MSBFIRST, SPI_MODE0));
+    SPI.beginTransaction(SPISettings(80000, LSBFIRST, SPI_MODE2));
     digitalWrite(PIN_BTN_LATCH,LOW);
     delayMicroseconds(100);
     digitalWrite(PIN_BTN_LATCH,HIGH);
     cli();
     for (int i = 0; i < NUM_REGISTERS; i++) {
         temp = ~SPI.transfer(0x00);
-        buttons_raw[i] = temp;
-        if (buttons_raw[i] != old_buttons[i]) something_happened = true;
+        buttons_raw |=  (temp << (i * 8)) ;//((temp << 4) & 0xF0 ) | ((temp >> 4) & 0x0F);
     }
     sei();
     SPI.endTransaction();
     
-    if (something_happened) {
+    if (old_buttons == buttons_raw) return;
+    
+    
+    long triggers =  ~old_buttons & buttons_raw;
+    
+    for (int i = 0; i < NUM_PIXELS; i++) {
+        if (triggers & (1 << i)) {
+            switch (i){
+                case 12:
+                    selected_menu = MENU_BRIGHTNESS;
+                    break;
+                case 13:
+                    selected_menu = MENU_BPM;
+                    break;
+               case 15:
+                    selected_menu = MENU_NONE;
+                    break;
+               case 22:
+                    selected_menu = MENU_TRIGGER_1;
+                    break;                    
+               case 21:
+                    selected_menu = MENU_TRIGGER_2;
+                    break;
+               case 20:
+                    selected_menu = MENU_TRIGGER_3;
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+    
+    
+    
+    last_ui_interaction = millis();
+    old_buttons = buttons_raw;
+    display_needs_update = true;
+    
+    
+  /*  if (something_happened) {
         byte triggers;
         for (int i = 0; i < NUM_REGISTERS; i++) {
            triggers =  ~old_buttons[i] & buttons_raw[i];
            if (triggers > 0) {
-
+            if (triggers & (1 << bit)){
            }
         }
     }
@@ -236,7 +310,7 @@ void check_buttons() {
     for (int i = 0; i < NUM_REGISTERS; i++) {
         old_buttons[i] = buttons_raw[i];
     }
-
+*/
     
 }
 
@@ -244,17 +318,35 @@ void check_buttons() {
 //																		Leds
 
 void update_leds() {
-	fill_solid(pixels,NUM_PIXELS,CRGB::Black);
-	
-	for (int n = 0; n < NUM_REGISTERS; n++) {
-	    for (int i = 0; i < 8; i++) {
-	        if (buttons_raw[n] & (1 << i)) {
-	            pixels[n*8 + i] = CRGB::Yellow;
-	        }
+	fill_solid(pixels,NUM_PIXELS,CRGB(0,5,10));
+	for (int i = 0; i < NUM_PIXELS; i++) {
+        if (buttons_raw & (1 << i)) {
+	        pixels[i] = CRGB::Yellow;
 	    }
 	}
 	
-	
+	switch(selected_menu){
+	    case MENU_BRIGHTNESS:
+	        pixels[12] = SELECTION_COLOR;
+	        break;
+	    case MENU_BPM:
+	        pixels[13] = SELECTION_COLOR;
+	        break;
+	    case MENU_TRIGGER_1:
+	        pixels[22] = SELECTION_COLOR;
+	        break;
+	    case MENU_TRIGGER_2:
+	        pixels[21] = SELECTION_COLOR;
+	        break;
+	    case MENU_TRIGGER_3:
+	        pixels[20] = SELECTION_COLOR;
+	        break;
+	}
+
+    if (digitalRead(PIN_TRIGGER_1)) {pixels[22] = CRGB::Blue;}
+    if (digitalRead(PIN_TRIGGER_2)) {pixels[21] = CRGB::Blue;}
+    if (digitalRead(PIN_TRIGGER_3)) {pixels[20] = CRGB::Blue;}
+
 	FastLED.show(); 
 }
 
@@ -309,6 +401,46 @@ void update_display() {
 			display.print(" bpm");
 
 		    break;
+		case MENU_BRIGHTNESS:
+			display.setCursor(0,0);
+	        display.print("BRIGHTNESS");
+
+			display.setTextSize(2);				
+			display.setCursor(0,14);
+			display.print(brightness);
+		    break;
+		case MENU_BPM:
+			display.setCursor(0,0);
+	        display.print("BPM");
+
+			display.setTextSize(2);				
+			display.setCursor(0,14);
+			display.print(bpm);
+		    break;
+		case MENU_TRIGGER_1:
+			display.setCursor(0,0);
+	        display.print("TRIGGER OUT 1");
+
+			display.setTextSize(2);				
+			display.setCursor(0,14);
+			display.print(trigger1_clocks);
+		    break;
+		case MENU_TRIGGER_2:
+			display.setCursor(0,0);
+	        display.print("TRIGGER OUT 2");
+
+			display.setTextSize(2);				
+			display.setCursor(0,14);
+			display.print(trigger2_clocks);
+		    break;
+		case MENU_TRIGGER_3:
+			display.setCursor(0,0);
+	        display.print("TRIGGER OUT 3");
+
+			display.setTextSize(2);				
+			display.setCursor(0,14);
+			display.print(trigger3_clocks);
+		    break;
 		default:
 		    break;
 	}
@@ -345,9 +477,10 @@ void check_clock(){
         if (tickCounter % trigger2_clocks == 0 ) digitalWrite(PIN_TRIGGER_2, HIGH);
         if (tickCounter % trigger3_clocks == 0 ) digitalWrite(PIN_TRIGGER_3, HIGH);
 
-        t.after(5,release_triggers);
+        t.after(10,release_triggers);
         update_display();           //display update needs a lot of time. do it right after clock tick
                                     // (if necessary) in order to not skew clock timing 
+        update_leds();
     }
 }
 
@@ -378,12 +511,15 @@ void setup(){
     Serial.begin(115200);
     Serial.println("vMIX20 - SETUP start");
 
+    brightness = 100;
+
     FastLED.addLeds<NEOPIXEL, PIN_PIXELS>(pixels, NUM_PIXELS);
-	FastLED.setBrightness(20);
+	FastLED.setBrightness(brightness);
 	
 	display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
 	display.setRotation(0); 
-	intro();
+//	intro();
+
 
     
     pinMode(PIN_TRIGGER_1, OUTPUT);
@@ -393,8 +529,11 @@ void setup(){
     pinMode(PIN_BTN_LATCH, OUTPUT);
     pinMode(PIN_RX, INPUT_PULLUP);    
     
-    encoder.attachHalfQuad(PIN_ENCODER_A, PIN_ENCODER_B);
+    SPI.begin();
+
     
+ //   encoder.attachHalfQuad(PIN_ENCODER_B, PIN_ENCODER_A);
+       encoder.attachSingleEdge(PIN_ENCODER_B, PIN_ENCODER_A);
      
     bpm = 113;
     sync_mode = SYNC_MODE_INTERN;
@@ -412,7 +551,7 @@ void setup(){
 
     // do this on vertical blanking:
     t.every(20,check_buttons);
-    t.every(20,update_leds);
+   // t.every(20,update_leds);
 
     
     Serial.println("SETUP done");
